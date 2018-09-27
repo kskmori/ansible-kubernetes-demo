cee1b1c296956ea7b691e29424474e5661fdedf1
Keisuke MORI <kskmori@intellilink.co.jp>
2018-09-27 19:22:43 +0900

    make container images reusable for Pacemaker bundle

--- roles/apps-images-prepare/files/db-pod/initdb.sh
+++ roles/apps-images-prepare/files/db-pod/initdb.sh
@@ -3,24 +3,30 @@
 PGDATA=/var/lib/pgsql/data
 export PGDATA
 
-if [ -r $PGDATA/PG_VERSION ]; then
-    echo "Database already initialized. Done."
-    exit 0
+if [ -e "$PGDATA/PG_VERSION" ]; then
+    echo "Database already initialized."
+else
+    # initialize the database
+    mkdir -p "$PGDATA"
+    chown -R postgres "$PGDATA"
+    chmod 700 "$PGDATA"
+    runuser postgres -c initdb || exit 1
+
+    # minimum PostgreSQL configuration
+    # BE CAREFUL!: this is only for demo
+
+    #  allow the database access from anywhere
+    echo "host all all all trust" >> $PGDATA/pg_hba.conf
+
+    #  listen on the network
+    echo "listen_addresses = '*'" >> $PGDATA/postgresql.conf
+
+
+    # intial table setup for the application
+    runuser postgres -c "pg_ctl start -w"
+    runuser postgres -c "psql -h localhost -U postgres -f init.sql"
+    runuser postgres -c "pg_ctl stop -w"
 fi
 
-runuser postgres -c initdb
-
-# minimum PostgreSQL configuration
-# BE CAREFUL!: this is only for demo
-
-#  allow the database access from anywhere
-echo "host all all all trust" >> $PGDATA/pg_hba.conf
-
-#  listen on the network
-echo "listen_addresses = '*'" >> $PGDATA/postgresql.conf
-
-
-# intial table setup for the application
-runuser postgres -c "pg_ctl start -w"
-runuser postgres -c "psql -h localhost -U postgres -f init.sql"
-runuser postgres -c "pg_ctl stop -w"
+echo "Database service is starting up."
+exec "$@"
